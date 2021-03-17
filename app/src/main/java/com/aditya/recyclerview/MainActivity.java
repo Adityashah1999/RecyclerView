b9561e72497fd8bd9255917957988c9faaed2266
Aditya Shah <nischalshah1999@gmail.com>
2021-03-18 01:10:56 +0545

    Task2 completed.

--- app/src/main/java/com/aditya/recyclerview/MainActivity.java
+++ app/src/main/java/com/aditya/recyclerview/MainActivity.java
@@ -7,6 +7,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
@@ -14,9 +16,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    // LinkedList to hold the data
     private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +34,26 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                int wordListSize = mWordList.size();
+
+                mWordList.addLast("+ Word " + wordListSize);
+
+                Objects.requireNonNull(mRecyclerView.getAdapter()).notifyItemInserted(wordListSize);
+                // scroll to the bottom of the list
+                mRecyclerView.smoothScrollToPosition(wordListSize);
             }
         });
 
-        for (int i = 0; i < 20; i++){
-            mWordList.addLast("Word " + 1);
 
+        for (int i = 1; i < 21; i++) {
+            mWordList.add("Word " + i);
         }
+
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        WordListAdapter mAdapter = new WordListAdapter(this, mWordList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
@@ -49,16 +65,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
