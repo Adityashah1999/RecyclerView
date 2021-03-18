157c37ad634f9868326517a776d33b3bd20fff2c
Aditya Shah <nischalshah1999@gmail.com>
2021-03-18 12:16:45 +0545

    Homework completed.

--- app/src/main/java/com/aditya/recyclerview/MainActivity.java
+++ app/src/main/java/com/aditya/recyclerview/MainActivity.java
@@ -1,81 +1,74 @@
 package com.aditya.recyclerview;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
 
+import java.io.InputStream;
 import java.util.LinkedList;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    // LinkedList to hold the data
-    private final LinkedList<String> mWordList = new LinkedList<>();
-    private RecyclerView mRecyclerView;
+    private static final String TAG = "MainActivity";
+    private final LinkedList<String> mRecipeNames = new LinkedList<>();
+    private final LinkedList<String> mRecipeDesc = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int wordListSize = mWordList.size();
-
-                mWordList.addLast("+ Word " + wordListSize);
-
-                Objects.requireNonNull(mRecyclerView.getAdapter()).notifyItemInserted(wordListSize);
-                // scroll to the bottom of the list
-                mRecyclerView.smoothScrollToPosition(wordListSize);
-            }
-        });
-
-
-        for (int i = 1; i < 21; i++) {
-            mWordList.add("Word " + i);
-        }
-
-
-        mRecyclerView = findViewById(R.id.recyclerView);
-        WordListAdapter mAdapter = new WordListAdapter(this, mWordList);
+        readFile(getResources().openRawResource(R.raw.recipe_names), mRecipeNames);
+        readFile(getResources().openRawResource(R.raw.recipe_desc), mRecipeDesc);
+        RecyclerView mRecyclerView = findViewById(R.id.recycler_view);
+        RecipeListAdapter mAdapter = new RecipeListAdapter(this, mRecipeNames, mRecipeDesc);
         mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.action_reset) {
-            mWordList.clear();
-            for (int i = 1; i < 21; i++){
-                mWordList.add("Word " + i);
-            }
-            Objects.requireNonNull(mRecyclerView.getAdapter()).notifyDataSetChanged();
-            return true;
+    private void readFile(InputStream ins, LinkedList<String> list) {
+        String contents = null;
+        String[] contents_array = null;
+        int i;
+        try {
+            contents = IOUtils.toString(ins);
+            System.out.println(contents);
+            IOUtils.closeQuietly(ins);
+        } catch (Exception e) {
+            Log.e(TAG, Log.getStackTraceString(e));
         }
-        return super.onOptionsItemSelected(item);
+
+        if (contents != null) {
+            contents_array = contents.split("\\r?\\n");
+        }
+        if (contents_array != null) {
+            for (i = 0; i < contents_array.length; i++) {
+                list.add(i, contents_array[i]);
+                Log.d("ARRAY CONTENTS", contents_array[i]);
+
+            }
+        }
+
+        }
+
+    private static class IOUtils {
+
+        public static void closeQuietly(InputStream ins) {
+
+        }
+
+        public static String toString(InputStream ins) {
+
+            return null;
+        }
     }
 }
+
+
+
